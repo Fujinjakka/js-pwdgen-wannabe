8c30b72a48b95a0285f4241828c92564237eb808
Fujinjakka <calvieri.marco@gmail.com>
2020-11-10 16:28:50 +0100

    exercise complete

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 
-var prova1 = prompt(chi sei?)
-console.log(prova1);
-document.getElementById('prova').innerHTML = prova1
-console.log(prova);
+var nome = prompt("come ti chiami?");
+console.log(nome);
+var cognome = prompt("qual'è il tuo cognome?");
+console.log(cognome);
+var colore = prompt("qual'è il tuo colore preferito?");
+console.log(colore);
+
+document.getElementById("password").innerHTML = nome + cognome + colore + "2020";
+console.log(password);
